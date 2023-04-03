b28b24cfa06df985f58e42150f667b082728f8c1
Caio Furman <caioxfurman@gmail.com>
2023-04-02 21:06:16 -0300

    Mantine component library added

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,19 +1,19 @@
 import { useState, useEffect } from "react";
 import '../styles/home.css'
-import api from '../api.js'
+// import api from '../api.js'
 
 function Home() {
 
-    const [parametros, setParametros] = useState([])
+    // const [parametros, setParametros] = useState([])
 
-    useEffect(() => {
-        api.get('/home').then(res => {
-            let teste = (res.data)
-            setParametros(teste)
-        })
-    }, [])
+    // useEffect(() => {
+    //     api.get('/home').then(res => {
+    //         let teste = (res.data)
+    //         setParametros(teste)
+    //     })
+    // }, [])
 
-    console.log(parametros)
+    // console.log(parametros)
 
     
 
@@ -21,7 +21,7 @@ function Home() {
     return (
         <div className="sei la">
             <h1>textooo</h1>
-            <h2>{parametros[0].teste}</h2>
+            <h2>teste</h2>
 
         </div>
     )
