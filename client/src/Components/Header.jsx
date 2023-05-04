6df862489b35c63fce24a471e6a649dca08ccca4
Caio Furman <caioxfurman@gmail.com>
2023-05-04 18:40:41 -0300

    Home and Footer created

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,9 +1,41 @@
 import * as React from 'react';
+import { Header, Text, Avatar, Anchor, Grid } from '@mantine/core';
+import Logo from '../Pages/Images/ublogoWhite.png';
 
-  export default function Header() {
-    return (
-      <div>
-
-      </div>
-    );
-  }
+export default function UbHeader() {
+  return (
+    <Header
+      style={{ backgroundColor: '#003F88'}}
+      shadow="sm"  
+      height={110}
+    >
+      <Grid
+      style={{paddingTop: 15 }}       
+      container
+      spacing={0}
+      justify="center">
+      <Grid style={{marginTop: 4, marginRight: 300}}>
+        <img src={Logo} className='footer-img' height='80px' width='100px'></img>
+      </Grid>
+        <Anchor href="/" style={{ marginRight: 25 }}>
+          <Text color={'white'} size={50} weight={700}>
+            Home
+          </Text>
+        </Anchor>
+        <Anchor href="/cursos" style={{ marginRight: 25 }}>
+          <Text color={'white'} size={50} weight={700}>
+            Cursos
+          </Text>
+        </Anchor>
+        <Anchor href="/suporte">
+          <Text color={'white'} size={50} weight={700}>
+            Suporte
+          </Text>
+        </Anchor>
+        <Grid style={{marginTop: 25, marginLeft: 300}}>
+          <Avatar radius="xl" />
+        </Grid>
+      </Grid>
+    </Header>
+  );
+}
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import { Grid, Text, Image, Button } from '@mantine/core';
 import '../styles/home.css';
-import Header from "../Components/Header"
+import UbHeader from "../Components/Header"
 import Footer from "../Components/Footer";
+import Logo from '../Pages/Images/UBlogo_alpha.png';
+import Banner from '../Pages/Images/Banner.png';
 // import api from '../api.js'
 
 function Home() {
@@ -18,14 +21,73 @@ function Home() {
 
     return (
         <div className="page">
-            <Header />
-            <div className="page-container">
-                <h1>textooo</h1>
-                <h2>teste</h2>
-            </div>
-            <Footer />
-        </div>
+            <UbHeader />
+            <Grid shadow="xl" style={{ position: 'relative' }}>
+                <img src={Banner} className='footer-img' height='900px' width='100%' alt='Banner' />
+                <Text
+                    color={'white'}
+                    shadow="xl" 
+                    size={50}
+                    weight={700}
+                    style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1 }}
+                >
+                    A maior plataforma de cursos online
+                </Text>
 
+                <Text
+                    color={'#cccccc'}
+                    shadow="xl" 
+                    size={40}
+                    weight={600}
+                    style={{ position: 'absolute', top: '57%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1 }}
+                >
+                    UniBrasil ACADEMY
+                </Text>
+            </Grid>
+            <Grid container justify="center">
+                <div className="cs-page-container2">
+
+                <Grid justify='center' style={{marginTop: '50px', marginBottom: '50px'}}>
+                        <Text color={'#003F88'} size={40} weight={700}>
+                            Cursos em alta
+                        </Text>
+                    </Grid>
+
+                    <Grid container justify='center' align='center' style={{ marginBottom: '125px', width: '100%' }}>
+                        <Grid justify='center' style={{ marginRight: '6%', backgroundColor: '#003F88', height: '200px', width: '250px', borderRadius: '25px'}}>
+                            <Text color={'white'} size={30} weight={500} style={{margin: 'auto'}}>
+                                Item 1
+                            </Text>
+                        </Grid>
+                        <Grid justify='center' style={{ marginRight: '6%', backgroundColor: '#003F88', height: '200px', width: '250px', borderRadius: '25px'}}>
+                            <Text color={'white'} size={30} weight={500} style={{margin: 'auto'}}>
+                                Item 2
+                            </Text>
+                        </Grid>
+                        <Grid justify='center' style={{ backgroundColor: '#003F88', height: '200px', width: '250px', borderRadius: '25px'}}>
+                            <Text color={'white'} size={30} weight={500} style={{margin: 'auto'}}>
+                                Item 3
+                            </Text>
+                        </Grid>
+                    </Grid>
+
+                    <Grid justify='center' style={{marginBottom: '10px'}}>
+                        <Text color={'gray'} size={30} weight={500}>
+                            Pesquise já o curso de sua escolha!
+                        </Text>
+                    </Grid>
+
+                    <div className="cs-search-div">
+                        <input className="search-input" type="text" placeholder="Pesquise seu curso..."></input>
+                    </div>
+                    
+                    <Grid justify='center' style={{marginTop: '110px', marginBottom: '70px'}}>
+                        <img src={Logo} className='footer-img' height='200px' width='350px'></img>
+                    </Grid>
+                </div>
+            </Grid>
+        <Footer />
+      </div>
     )
 }
 
