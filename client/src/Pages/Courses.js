c69cda3bb19467f16be5502a462401d3ae099ebe
LucasLMartins <lucasluh.morais@hotmail.com>
2023-05-11 23:23:56 -0300

    Courses/Lesson pages complete
    Courses page and Lesson page front-end

--- client/src/Pages/Courses.js
+++ client/src/Pages/Courses.js
@@ -3,10 +3,55 @@ import '../styles/courses.css';
 import Header from "../Components/Header"
 import Footer from "../Components/Footer";
 // import api from '../api.js'
+import ArrowDown from "./Images/arrow.png";
+import ArrowUp from "./Images/arrowUp.png";
+import { useNavigate } from "react-router-dom";
 
 function Courses() {
+    const navigate = useNavigate()
 
+    document.title = "Cursos"
 
+    let categoryArrowState = 1;
+    let priceArrowState = 0;
+
+    function clickCategory(){
+        const categoryArrow = document.getElementById('categoryArrowId');
+        const categoryOptions = document.getElementById('categoryOptionsId');
+
+        if(categoryArrowState == 0){
+            categoryArrow.src = ArrowUp
+            categoryArrowState = 1
+            categoryOptions.style.display = 'block';
+        }
+        else if(categoryArrowState == 1){
+            categoryArrow.src = ArrowDown
+            categoryArrowState = 0
+            categoryOptions.style.display = 'none';
+        }
+    }
+
+    function clickPrice(){
+        const priceArrow = document.getElementById('priceArrowId');
+        const priceOptions = document.getElementById('priceOptionsId');
+
+        if(priceArrowState == 0){
+            priceArrow.src = ArrowUp
+            priceArrowState = 1
+            priceOptions.style.display = 'block';
+        }
+        else if(priceArrowState == 1){
+            priceArrow.src = ArrowDown
+            priceArrowState = 0
+            priceOptions.style.display = 'none';
+        }
+    }
+
+    function clearFilters(){
+        //document.getElementById('range-max').value = ''
+        //document.getElementById('range-min').value = ''
+        navigate('/cursos')
+    }
 
     return (
         <div className="page">
@@ -21,16 +66,17 @@ function Courses() {
                         <h1 className="cs-title">Cursos</h1>
                     </div>
                 </div>
+
                 <div className="cs-bottom-div">
                     <div className="cs-bottom-div-left">
                         <h2 className="cs-filter-title">Filtrar por</h2>
                         <div className="cs-filter-div">
                             <div className="cs-filter-cat">
-                                <div className="cs-filter-titles">
+                                <div className="cs-filter-titles" onClick={() => clickCategory()}>
                                     <h4>Categoria</h4>
-                                    <img src={require('./Images/arrowUp.png')} width="40px" height="40px"></img>
+                                    <img id="categoryArrowId" src={ArrowUp} width="40px" height="40px"></img>
                                 </div>
-                                <div className="cs-filter-cat-content">
+                                <div className="cs-filter-cat-content" id="categoryOptionsId">
                                     <ul className="">
                                         <li className="cs-filter-li">
                                             <p className="cs-filter-option">Javascript</p>
@@ -45,16 +91,26 @@ function Courses() {
                             </div>
 
                             <div className="cs-filter-price">
-                                <div className="cs-filter-titles">
+                                <div className="cs-filter-titles" onClick={() => clickPrice()}>
                                     <h4>Preço</h4>
-                                    <img src={require('./Images/arrow.png')} width="40px" height="40px"></img>
+                                    <img id="priceArrowId" src={require('./Images/arrow.png')} width="40px" height="40px"></img>
                                 </div>
-                                <div className="cs-filter-price-content">
-                                    
+                                <div className="cs-filter-price-content" id="priceOptionsId">
+                                    <div className="filter-by-price-min">
+                                        <p className="filter-by-price-from">De:</p>
+                                        <input id="range-min" className="input-from-price" type="number" placeholder="0.00"></input>
+                                    </div>
+                                    <div className="filter-by-price-max">
+                                        <p className="filter-by-price-from">Até:</p>
+                                        <input id="range-max" className="input-from-price" type="number" placeholder="0.00"></input>
+                                    </div>
+                                    <div className="filter-by-price-button-div">
+                                        <button className="filter-by-price-button">Aplicar</button>
+                                    </div>
                                 </div>
                             </div>
 
-                            <div className="cs-clear-filters">
+                            <div className="cs-clear-filters" onClick={() => clearFilters()}>
                                 <h3>Limpar Filtros</h3>
                             </div>
                         </div>
@@ -66,22 +122,99 @@ function Courses() {
                         </div>
                         <div className="cs-grid-div">
                             <div className="cs-grid-item-container">
-                                <a>Item 1</a>
+                                <a>
+                                    <div className="cs-img-container">
+                                        <img src={require('./Images/CostumerSupport.jpg')} className="cs-item-img"></img>
+                                    </div>
+                                    <div className="cs-item-name-container">
+                                        <p className="cs-item-name">Curso de JavaScript e TypeScript do básico ao avançado</p>
+                                    </div>
+                                    <div className="cs-item-price-container">
+                                        <div className="cs-item-price-container-inner">
+                                            <p className="cs-item-price">R$125,00</p>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
+
                             <div className="cs-grid-item-container">
-                                <a>Item 2</a>
+                                <a>
+                                    <div className="cs-img-container">
+                                        <img src={require('./Images/mapa.png')} className="cs-item-img"></img>
+                                    </div>
+                                    <div className="cs-item-name-container">
+                                        <p className="cs-item-name">Curso de JavaScript e TypeScript do básico ao avançado</p>
+                                    </div>
+                                    <div className="cs-item-price-container">
+                                        <div className="cs-item-price-container-inner">
+                                            <p className="cs-item-price">R$125,00</p>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
+
                             <div className="cs-grid-item-container">
-                                <a>Item 3</a>
+                                <a>
+                                    <div className="cs-img-container">
+                                        <img src={require('./Images/ublogoWhite.png')} className="cs-item-img"></img>
+                                    </div>
+                                    <div className="cs-item-name-container">
+                                        <p className="cs-item-name">Curso de JavaScript e TypeScript do básico ao avançado</p>
+                                    </div>
+                                    <div className="cs-item-price-container">
+                                        <div className="cs-item-price-container-inner">
+                                            <p className="cs-item-price">R$125,00</p>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
+
                             <div className="cs-grid-item-container">
-                                <a>Item 4</a>
+                                <a>
+                                    <div className="cs-img-container">
+                                        <img src={require('./Images/mapa.png')} className="cs-item-img"></img>
+                                    </div>
+                                    <div className="cs-item-name-container">
+                                        <p className="cs-item-name">Curso de JavaScript e TypeScript do básico ao avançado</p>
+                                    </div>
+                                    <div className="cs-item-price-container">
+                                        <div className="cs-item-price-container-inner">
+                                            <p className="cs-item-price">R$125,00</p>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
+
                             <div className="cs-grid-item-container">
-                                <a>Item 5</a>
+                                <a>
+                                    <div className="cs-img-container">
+                                        <img src={require('./Images/CostumerSupport.jpg')} className="cs-item-img"></img>
+                                    </div>
+                                    <div className="cs-item-name-container">
+                                        <p className="cs-item-name">Curso de JavaScript e TypeScript do básico ao avançado</p>
+                                    </div>
+                                    <div className="cs-item-price-container">
+                                        <div className="cs-item-price-container-inner">
+                                            <p className="cs-item-price">R$125,00</p>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
+
                             <div className="cs-grid-item-container">
-                                <a>Item 6</a>
+                                <a>
+                                    <div className="cs-img-container">
+                                        <img src={require('./Images/mapa.png')} className="cs-item-img"></img>
+                                    </div>
+                                    <div className="cs-item-name-container">
+                                        <p className="cs-item-name">Curso de JavaScript e TypeScript do básico ao avançado</p>
+                                    </div>
+                                    <div className="cs-item-price-container">
+                                        <div className="cs-item-price-container-inner">
+                                            <p className="cs-item-price">R$125,00</p>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
                         </div>
                     </div>
--- client/src/Pages/Lesson.js
+++ client/src/Pages/Lesson.js
@@ -16,33 +16,44 @@ function Lesson() {
             <div className="les-page-container">
                 <div className="les-left-div">
                     <div className="les-left-top-div">
-                        <h2>Introdução ao curso</h2>
+                        <a href="/perfil" className="return-div">
+                            <img src={require('./Images/arrowLeft.png')}></img>
+                        </a>
+                        <div className="les-left-top-title">
+                            <h2 className="les-title">Curso de Banco de Dados</h2>
+                            <h3 className="les-subtitle">Introdução ao curso</h3>
+                        </div>
+                        
                     </div>
                     <div className="les-video-div">
-                    <ReactPlayer
-                        className=''
-                        width="100%"
-                        height="800px"
-                        url= 'https://www.youtube.com/watch?v=CQmX6GcpyvE&list=PLUae-M0w5U8dpoTgg15G2o2ypnCGoWXAU&ab_channel=Prof.FabioE.Santos'
-                        controls = {true}
-                    />
+                        <ReactPlayer
+                            className=''
+                            width="100%"
+                            height="100%"
+                            url= 'https://www.youtube.com/watch?v=CQmX6GcpyvE&list=PLUae-M0w5U8dpoTgg15G2o2ypnCGoWXAU&ab_channel=Prof.FabioE.Santos'
+                            controls = {true}
+                        />
                     </div>
                     <div className="les-bottom-div">
 
                     </div>
                 </div>
+
                 <div className="les-right-div">
                     <div className="les-course-title">
                         <h3>Curso de Banco de Dados</h3>
                     </div>
                     <div className="les-lessons">
                         <div className="les-lesson-container">
+                            <img className="play-icon" src={require('./Images/playArrow2.png')}></img>
                             <h4>Introdução ao curso</h4>
                         </div>
-                        <div className="les-lesson-container">
+                        <div className="les-lesson-container les-lesson-selected">
+                            <img className="play-icon" src={require('./Images/playArrow2.png')}></img>
                             <h4>Aula 1</h4>
                         </div>
                         <div className="les-lesson-container">
+                            <img className="play-icon" src={require('./Images/playArrow2.png')}></img>
                             <h4>Aula 2</h4>
                         </div>
                     </div>
